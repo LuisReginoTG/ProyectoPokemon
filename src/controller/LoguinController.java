148d3f54a7c36d88570a2ac865ce78e61789d569
LuisReginoTG <luis.regino.tg@gmail.com>
2024-04-16 11:19:40 +0200

    Base de datos
    Base de datos

--- src/controller/LoguinController.java
+++ src/controller/LoguinController.java
@@ -12,52 +12,88 @@ import javafx.stage.Stage;
 
 public class LoguinController {
 
-    @FXML
-    private Button btnAceptar;
+	@FXML
+	private Button btnAceptar;
 
-    @FXML
-    private Button btnCancelar;
+	@FXML
+	private Button btnCancelar;
 
-    @FXML
-    private Button btnRegistrar;
+	@FXML
+	private Button btnRegistrar;
 
-    @FXML
-    private ImageView imgFondoLoguin;
+	@FXML
+	private ImageView imgFondoLoguin;
 
-    @FXML
-    private ImageView imgLogoLoguin;
+	@FXML
+	private ImageView imgLogoLoguin;
 
-    @FXML
-    private Label lblPass;
+	@FXML
+	private Label lblPass;
 
-    @FXML
-    private Label lblUsuario;
+	@FXML
+	private Label lblUsuario;
+	
+	@FXML
+	private Label lblError;
 
-    @FXML
-    private PasswordField txtPass;
+	@FXML
+	private PasswordField txtPass;
 
-    @FXML
-    private TextField txtUsuario;
+	@FXML
+	private TextField txtUsuario;
 
-    @FXML
-    public void cerrarAplicacion(ActionEvent event) {
-    	Stage stage = (Stage) btnCancelar.getScene().getWindow();
-    	stage.close();
-    }
+	@FXML
+	public void cerrarAplicacion(ActionEvent event) {
+		Stage stage = (Stage) btnCancelar.getScene().getWindow();
+		stage.close();
+	}
 
-    @FXML
-    public void eventKey(KeyEvent event) {
+	@FXML
+	private void eventKey(KeyEvent event) {
+//		Object evt = event.getSource();
+//
+//		if (evt.equals(txtUsuario)) {
+//			if (event.getCharacter().equals(" ")) {
+//				event.consume();// Detenemos el evento
+//			}
+//		} else if (evt.equals(txtPass)) {
+//			if (event.getCharacter().equals(" ")) {
+//				event.consume();// Detenemos el evento
+//			}
+//		}
+		
+		if (event.getCharacter().equals(" ")) {
+			event.consume();// Detenemos el evento
+		}
+	}
 
-    }
+	@FXML
+	public void loguearse(ActionEvent event) {
+		Object evt  = event.getSource();
+		
+		if(txtUsuario.getText().isEmpty()) {
+			lblError.setText("Error: Inserta nombre de usuario");
+			lblError.setVisible(true);
+		}else if(txtPass.getText().isEmpty()) {
+			lblError.setText("Error: Inserta contraseña");
+			lblError.setVisible(true);
+		}else {
+			String usuario = txtUsuario.getText();
+			String pass = txtPass.getText();
+			
+			String sql ="SELECT COUNT(*)\n"
+					+ "FROM ENTRENADOR\n"
+					+ "WHERE NOM_ENTRENADOR = ?\n"
+					+ "AND PASSWORD = ?";
+			
+			
+			
+		}
+	}
 
-    @FXML
-    public void loguearse(ActionEvent event) {
+	@FXML
+	void registrar(ActionEvent event) {
 
-    }
-
-    @FXML
-    void registrar(ActionEvent event) {
-
-    }
+	}
 
 }
