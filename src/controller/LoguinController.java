560ecf644b1c1940d636a5c7e88e3fbd9483a6d7
LuisReginoTG <luis.regino.tg@gmail.com>
2024-04-16 09:56:54 +0200

    img, fxml, controller
    Se añade img, se crean fxml y controladores básicos

---
+++ src/controller/LoguinController.java
@@ -0,0 +1,59 @@
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.KeyEvent;
+
+public class LoguinController {
+
+    @FXML
+    private Button btnAceptar;
+
+    @FXML
+    private Button btnCancelar;
+
+    @FXML
+    private Button btnRegistrar;
+
+    @FXML
+    private ImageView imgFondoLoguin;
+
+    @FXML
+    private ImageView imgLogoLoguin;
+
+    @FXML
+    private Label lblPass;
+
+    @FXML
+    private Label lblUsuario;
+
+    @FXML
+    private PasswordField txtPass;
+
+    @FXML
+    private TextField txtUsuario;
+
+    @FXML
+    void cerrarAplicacion(ActionEvent event) {
+
+    }
+
+    @FXML
+    void eventKey(KeyEvent event) {
+
+    }
+
+    @FXML
+    void loguearse(ActionEvent event) {
+
+    }
+
+    @FXML
+    void registrar(ActionEvent event) {
+
+    }
+
+}
---
+++ src/controller/PrincipalController.java
@@ -0,0 +1,34 @@
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.image.ImageView;
+
+public class PrincipalController {
+
+    @FXML
+    private Button btnCapturar;
+
+    @FXML
+    private Button btnCombate;
+
+    @FXML
+    private Button btnEntrenar;
+
+    @FXML
+    private Button btnEquipo;
+
+    @FXML
+    private Button btnHospital;
+
+    @FXML
+    private Button btnTienda;
+
+    @FXML
+    private ImageView imgFondoPrincipal;
+
+    @FXML
+    private ImageView imgSalir;
+
+    @FXML
+    private ImageView imgTituloPrincipal;
+
+}
